1bcf8058cbc6297f42a4c53db32ffbde5b8595c9
Paul Walker <paul@blacksun.org.uk>
2011-11-09 15:10:50 +0000

    now splitting and should be hashing, failing to extract hash though

--- chunker.c
+++ chunker.c
@@ -7,6 +7,7 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <fcntl.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
@@ -121,6 +122,7 @@ static int open_file(const char *filename, file_info *file, bool reading, bool c
         /* Default */
         file->fd = -1;
         file->start_buf = MAP_FAILED;
+        file->temp_buf = NULL;
 
         if (reading)
         {
@@ -129,14 +131,12 @@ static int open_file(const char *filename, file_info *file, bool reading, bool c
                         printf("Couldn't get filesize of input file '%s'\n", filename);
                         return -1;
                 }
+                flags = O_RDONLY;
         }
         else
+        {
                 file->size = -1;
 
-        if (reading)
-                flags = O_RDONLY;
-        else
-        {
                 flags = O_RDWR;
                 if (create)
                         flags |= O_CREAT | O_TRUNC;
@@ -147,33 +147,34 @@ static int open_file(const char *filename, file_info *file, bool reading, bool c
         if (file->fd < 0)
         {
                 perror("Couldn't open file");
+                return -1;
         }
 
-        /* Try mmap */
         if (reading)
-                flags = PROT_READ;
-        else
-                flags = PROT_WRITE;
-        file->start_buf = mmap(0, file->size, flags, MAP_SHARED, file->fd, 0);
-        if (MAP_FAILED == file->start_buf)
         {
-                printf("Couldn't mmap file, will access the slow way.\n");
-                file->temp_buf = malloc(bufsize);
-                if (NULL == file->temp_buf)
+                /* Try mmap */
+                flags = PROT_READ;
+                file->start_buf = mmap(0, file->size, PROT_READ, MAP_SHARED, file->fd, 0);
+                if (MAP_FAILED == file->start_buf)
                 {
-                        printf("Couldn't allocate %i bytes for temporary memory.\n", bufsize);
-                        close(file->fd);
-                        file->fd = -1;
+                        printf("Couldn't mmap file, will access the slow way.\n");
+                        file->temp_buf = malloc(bufsize);
+                        if (NULL == file->temp_buf)
+                        {
+                                printf("Couldn't allocate %i bytes for temporary memory.\n", bufsize);
+                                close(file->fd);
+                                file->fd = -1;
+                        }
+                        else
+                        {
+                                printf("Allocated temporary buffer at %p\n", file->temp_buf);
+                        }
                 }
                 else
                 {
-                        printf("Allocated temporary buffer at %p\n", file->temp_buf);
+                        printf("Successfully mmapped input to %p\n", file->start_buf);
+                        file->temp_buf = NULL;
                 }
-        }
-        else
-        {
-                printf("Successfully mmapped input to %p\n", file->start_buf);
-                file->temp_buf = NULL;
         }
 
         file->offset = 0;
@@ -209,9 +210,9 @@ static int close_file(file_info *file)
         return 0;
 }
 
-static int file_next_buffer(file_info *file, char **buf, unsigned int *count)
+static int file_read_next_buffer(file_info *file, char **buf, signed int toread, unsigned int *count)
 {
-        ssize_t bytesread;
+        ssize_t bytesread, bytestoread;
 
         assert(file);
         assert(file->fd >= 0);
@@ -226,17 +227,24 @@ static int file_next_buffer(file_info *file, char **buf, unsigned int *count)
                 return 0;
         }
 
+        if (toread < 0)
+                bytestoread = file->bufsize;
+        else
+                bytestoread = (ssize_t)toread;
+
+        printf("Reading %i bytes.\n", bytestoread);
+
         /* TODO implement common offset/size checking to ensure getting next buffer makes sense */
 
         /* See if we can do it the easy way */
         if (file->start_buf)
         {
                 printf("File is mapped, easy.\n");
-                printf("start_buf %p bufsize %i\n", file->start_buf, file->bufsize);
+                printf("start_buf %p bufsize %i\n", file->start_buf, bytestoread);
                 *buf = file->start_buf + file->offset;
                 printf("Returning %p\n", *buf);
-                if ((file->size - file->offset) > file->bufsize)
-                        bytesread = file->bufsize;
+                if ((file->size - file->offset) > bytestoread)
+                        bytesread = bytestoread;
                 else
                         bytesread = (file->size - file->offset);
         }
@@ -245,7 +253,7 @@ static int file_next_buffer(file_info *file, char **buf, unsigned int *count)
                 printf("File isn't mapped, less easy.\n");
 
                 /* For speed, assume we're the only ones using this file pointer. */
-                bytesread = read(file->fd, file->temp_buf, file->bufsize);
+                bytesread = read(file->fd, file->temp_buf, bytestoread);
                 if (0 == bytesread)
                 {
                         printf("Got 0 for bytesread, EOF.\n");
@@ -254,9 +262,9 @@ static int file_next_buffer(file_info *file, char **buf, unsigned int *count)
                                 printf("File offset is %lld, size is %lld - unexpected EOF.\n", file->offset, file->size);
                         }
                 }
-                else if (bytesread != file->bufsize)
+                else if (bytesread != bytestoread)
                 {
-                        printf("Bytes read %i doesn't match bufsize %i\n", bytesread, file->bufsize);
+                        printf("Bytes read %i doesn't match bufsize %i\n", bytesread, bytestoread);
                 }
 
         }
@@ -267,15 +275,98 @@ static int file_next_buffer(file_info *file, char **buf, unsigned int *count)
         return 0;
 }
 
+static int file_write_next_buffer(file_info *file, const char *buf, unsigned int count)
+{
+        int retval = 0;
+        ssize_t written;
+
+        assert(file);
+        assert(buf);
+
+        if (file->fd < 0)
+        {
+                printf("Bad parameter, file not open\n");
+                return -1;
+        }
+
+        written = write(file->fd, buf, count);
+        if (0 == written)
+        {
+                printf("Wrote 0 bytes, expected to write %i. Disk full?\n", count);
+                retval = -1;
+        }
+        else if (written != count)
+        {
+                printf("Warning - expected to write %d bytes, only wrote %d. Disk becoming full?\n", written, count);
+                retval = -2;
+        }
+
+        return retval;
+}
+
 /**** Main functions */
+
+static int write_context_hash(const char *basename, const char *extension, CRYPT_CONTEXT *context)
+{
+        int buflen, fd, hash_length, rc;
+        char *name;
+        char hash[CRYPT_MAX_HASHSIZE];
+
+        assert(basename);
+        assert(extension);
+        assert(context);
+
+        rc = cryptGetAttributeString(*context, CRYPT_CTXINFO_HASHVALUE, hash, &hash_length);
+        if (cryptStatusError(rc))
+        {
+                printf("Couldn't get hash from context - rc %i\n", rc);
+                return rc;
+        }
+
+        buflen = strlen(basename) + strlen(extension) + 1;
+        name = malloc(buflen);
+        if (NULL == name)
+        {
+                printf("Couldn't allocate %i byte name buffer.\n", buflen);
+                return -1;
+        }
+
+        fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0622);
+        if (fd < 0)
+        {
+                printf("Unable to open file '%s' for writing.\n", name);
+        }
+        else
+        {
+                rc = write(fd, hash, hash_length);
+                if (rc != hash_length)
+                {
+                        printf("Expected to write %d bytes, only wrote %d.\n", hash_length, rc);
+                        perror("Why");
+                }
+                else
+                {
+                        printf("Hash written okay.\n");
+                }
+
+                close(fd);
+        }
+
+        free(name);
+        return 0;
+}
 
 int split_file(CRYPT_CONTEXT *md5, CRYPT_CONTEXT *sha1, const char *infilename, const char *outfilenamebase, unsigned long long int chunksize)
 {
         char *buf;
+        char *outfilename;
         file_info infile;
         file_info outfile;
-        int rc;
-        unsigned int count;
+        int rc, outfilenamelen;
+        signed int to_read;
+        unsigned int bytecount;
+        unsigned long long int bytesleftinchunk;
+        unsigned int chunkcount = 0;
 
         rc = 0;
         if (!infilename)
@@ -301,33 +392,112 @@ int split_file(CRYPT_CONTEXT *md5, CRYPT_CONTEXT *sha1, const char *infilename, 
                 return -1;
         }
 
-        /* TODO build up chunk names */
-        if (open_file("test.bin", &outfile, false, true, 7500))
+        /* Filename, ., make wild assumption of no more than 100000 chunks. */
+        outfilenamelen = strlen(infilename) + 1 + 6 + 1;
+        outfilename = (char *)malloc(outfilenamelen);
+        if (NULL == outfilename)
+        {
+                printf("Couldn't allocate memory for output filenames\n");
+                close_file(&infile);
+                return -1;
+        }
+
+        if (snprintf(outfilename, outfilenamelen, "%s%i", outfilenamebase, chunkcount) == outfilenamelen)
+        {
+                printf("Output filename too long (hit buffer size of %i bytes\n", outfilenamelen);
+                printf("Stopping now to avoid data issues.\n");
+                close_file(&infile);
+                free(outfilename);
+                return -1;
+        }
+
+        printf("Writing to file %s\n", outfilename);
+        if (open_file(outfilename, &outfile, false, true, 7500))
         {
                 printf("Couldn't open output file for writing\n");
                 close_file(&infile);
                 return -1;
         }
 
-#if 0
-        file_next_buffer(&infile, &buf, &count);
-        printf("Got buffer %p, count %i\n", buf, count);
-        file_next_buffer(&infile, &buf, &count);
-        printf("Got buffer %p, count %i\n", buf, count);
-#else
-        do
+        rc = 0;
+        bytesleftinchunk = chunksize;
+        printf("Comparing %i to %lld\n", infile.bufsize, bytesleftinchunk);
+        if (infile.bufsize > bytesleftinchunk)
+                to_read = bytesleftinchunk;
+        else
+                to_read = -1;
+        printf("Reading %i bytes.\n", to_read);
+        
+        file_read_next_buffer(&infile, &buf, to_read, &bytecount);
+        while ((bytecount > 0) && (rc == 0))
         {
-                file_next_buffer(&infile, &buf, &count);
-                printf("Got buffer %p, count %i\n", buf, count);
-                if (count && (write(outfile.fd, buf, count) != count))
+                printf("Got buffer %p, bytecount %i\n", buf, bytecount);
+                rc = file_write_next_buffer(&outfile, buf, bytecount);
+                if (rc != 0)
                 {
-                        printf("Didn't write expected number of bytes.\n");
+                        printf("Problem writing: ");
+                        switch (rc)
+                        {
+                                case -1: printf("disk may be full.\n");
+                                         break;
+                                default:
+                                case -2: printf("unknown write error.\n");
+                                         break;
+                        }
                 }
-        } while (count > 0);
-#endif
 
+                rc = cryptEncrypt(*md5, buf, bytecount);
+                if (cryptStatusError(rc))
+                {
+                        printf("Couldn't update MD5 hash context - rc %i\n", rc);
+                        break;
+                }
+                rc = cryptEncrypt(*sha1, buf, bytecount);
+                if (cryptStatusError(rc))
+                {
+                        printf("Couldn't update SHA-1 hash context - rc %i\n", rc);
+                        break;
+                }
+
+                bytesleftinchunk -= bytecount;
+                if (0 == bytesleftinchunk)
+                {
+                        printf("Finished chunk %i.\n", chunkcount);
+                        close_file(&outfile);
+                        write_context_hash(outfilename, "md5", md5);
+                        write_context_hash(outfilename, "sha1", sha1);
+
+                        bytesleftinchunk = chunksize;
+                        chunkcount++;
+
+                        if (snprintf(outfilename, outfilenamelen, "%s%i", outfilenamebase, chunkcount) == outfilenamelen)
+                        {
+                                printf("Output filename too long (hit buffer size of %i bytes\n", outfilenamelen);
+                                printf("Stopping now to avoid data issues.\n");
+                                break;
+                        }
+                        printf("Writing to file %s\n", outfilename);
+                        if (open_file(outfilename, &outfile, false, true, 7500))
+                        {
+                                printf("Couldn't open output file for writing\n");
+                                break;
+                        }
+                }
+                printf("Comparing %i to %lld\n", infile.bufsize, bytesleftinchunk);
+                if (infile.bufsize > bytesleftinchunk)
+                        to_read = bytesleftinchunk;
+                else
+                        to_read = -1;
+                printf("Reading %i bytes.\n", to_read);
+
+                file_read_next_buffer(&infile, &buf, to_read, &bytecount);
+        } while ((bytecount > 0) && (rc == 0));
+
+        free(outfilename);
         close_file(&infile);
         close_file(&outfile);
+        write_context_hash(outfilename, "md5", md5);
+        write_context_hash(outfilename, "sha1", sha1);
 
         return 0;
 }
@@ -367,7 +537,6 @@ int main(int argc, char *argv[])
         free_context(md5);
         free_context(sha1);
 
-/* cryptlib_setup_exit: */
         rc = cryptEnd();
         if (cryptStatusError(rc))
         {
